398615be597a2513012dc9ad84f9b8507d74d1cd
KanedaXIII <mnemosineii@gmail.com>
2015-12-12 20:50:18 +0100

    Add scripts Food, Sumo and scene Restaurant

--- LudumDare34/Assets/Scripts/Food.cs
+++ LudumDare34/Assets/Scripts/Food.cs
@@ -3,13 +3,46 @@ using System.Collections;
 
 public class Food : MonoBehaviour {
 
+    public string nameFood;
+    public int valueStrength;
+    public int valueResistence;
+    public int valueDefense;
+    public bool isTake=false;
+
 	// Use this for initialization
 	void Start () {
-	
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S))
+        {
+            setSumStats();
+            Destroy(this);
+        }
 	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag=="canTake")
+        {
+            this.isTake = true;
+        }else if (coll.gameObject.tag=="cantTake")
+        {
+            this.isTake = false;
+        }
+    }
+    public void setSumStats()
+    {
+        /*
+       
+        GameManager.instance.BonusStrength+=this.valueStrength;
+
+        GameManager.instance.BonusDefense+= this.valueDefense;
+
+        GameManager.instance.BonusResistence+=this.valueResistence;
+        */
+    }
 }
+
---
+++ LudumDare34/Assets/Scripts/Sumo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Sumo : MonoBehaviour {
+
+    public float strength;
+    public float resistence;
+    public float defense;
+
+    Scene sceneLoaded;
+
+    // Use this for initialization
+    void Start () {
+        sceneLoaded= SceneManager.GetActiveScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (sceneLoaded.name=="Restaurant")
+        {
+            if (Input.GetKeyDown(KeyCode.A)||Input.GetKeyDown(KeyCode.S)){
+
+            }
+        }else if (sceneLoaded.name == "Tatami")
+        {
+
+        }
+	}
+}
